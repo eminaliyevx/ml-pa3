eb288bf9ca0fff5cd699dfb3cc6f2efe1e546e50
eminaliyevx <eminaliyevx@gmail.com>
2022-11-25 20:22:43 +0400

    Implement gradient descent

--- task.py
+++ task.py
@@ -3,6 +3,7 @@ import numpy as np
 import utils
 import visualization
 from sklearn import linear_model
+import matplotlib.pyplot as plt
 
 # 1. Loading data
 df = pd.read_csv("turboaz.csv", usecols=["Yurush", "Buraxilish ili", "Qiymet"])
@@ -15,19 +16,28 @@ df["Qiymet"] = df["Qiymet"].map(lambda qiymet: utils.convertPrice(qiymet))
 
 # 3. Implementation of Linear Regression from scratch
 # Normalize data using Z score normalization
-X = df[["Yurush", "Buraxilish ili"]]
-Y = df["Qiymet"]
+x1 = df["Yurush"]
+x2 = df["Buraxilish ili"]
+y = df["Qiymet"]
 
-Y = np.array((Y - Y.mean()) / Y.std())
-X = X.apply(lambda rec: (rec - rec.mean()) / rec.std(), axis=0)
+x1 = np.array((x1 - x1.mean()) / x1.std())
+x2 = np.array((x2 - x2.mean()) / x2.std())
+x = np.c_[x1, x2, np.ones(x1.shape[0])]
 
 # Implement gradient descent algorithm to minimize cost function
-p = utils.gradientDescent(X["Yurush"], X["Buraxilish ili"], Y)
+weights = np.random.rand(3)
+weightList, costList = utils.gradientDescent(x, y, weights)
 
-print(p[-1])
-print(p[-1][0] + p[-1][1] * 240000 + p[-1][2] * 2000)
+print(np.dot([[240000, 2000, 1], [415558, 1996, 1]], weightList[-1]))
 
 # 4. Linear Regression using library
-# reg = linear_model.LinearRegression()
-# reg.fit(df[["Yurush", "Buraxilish ili"]], df["Qiymet"])
-# print(reg.coef_)
+reg = linear_model.LinearRegression()
+reg.fit(df[["Yurush", "Buraxilish ili"]], df["Qiymet"])
+
+print(reg.predict([[240000, 2000]]))
+
+# plt.title("Cost function")
+# plt.xlabel("Number of iterations")
+# plt.ylabel("Cost")
+# plt.plot(costList)
+# plt.show()
--- utils.py
+++ utils.py
@@ -18,24 +18,6 @@ def convertPrice(priceAsString):
     return price
 
 
-# Generate random bias and weights
-def generateWeights(n):
-    weights = np.random.rand(n)
-
-    return weights
-
-
-# Calculate X (predictions)
-def calculatePredictions(x1, x2, w0, w1, w2):
-    n = len(x1)
-    X = np.zeros(n)
-
-    for i in range(n):
-        X[i] = (w1 * x1[i]) + (w2 * x2[i]) + w0
-
-    return X
-
-
 # Calculate cost function
 def costFunction(x, y):
     cost = np.sum((x - y) ** 2) / (2 * len(x))
@@ -43,49 +25,20 @@ def costFunction(x, y):
     return cost
 
 
-# Calculate gradients
-def gradients(x1, x2, y, w0, w1, w2):
-    n = len(x1)
-    dj_dw0 = 0
-    dj_dw1 = 0
-    dj_dw2 = 0
-
-    for i in range(n):
-        h = (w1 * x1[i]) + (w2 * x2[i]) + w0
-        dj_dw0_i = h - y[i]
-        dj_dw1_i = (h - y[i]) * x1[i]
-        dj_dw2_i = (h - y[i]) * x2[i]
-
-        dj_dw0 += dj_dw0_i
-        dj_dw1 += dj_dw1_i
-        dj_dw2 += dj_dw2_i
-
-    dj_dw0 /= n
-    dj_dw1 /= n
-    dj_dw2 /= n
-
-    return dj_dw0, dj_dw1, dj_dw2
-
-
 # Implement gradient descent algorithm to minimize cost function
-def gradientDescent(x1, x2, y, alpha=0.001, iterations=10000):
-    n = len(x1)
-    alpha = alpha
-    iterations = iterations
-    w0 = 0
-    w1 = 0
-    w2 = 0
+def gradientDescent(x, y, weights, alpha=0.001, iterations=10000):
+    n = len(y)
+    weightList = [weights]
+    costList = []
 
-    p = []
+    for i in range(iterations):
+        prediction = np.dot(x, weights)
+        error = prediction - y
+        cost = 1 / (2 * n) * np.dot(error.T, error)
+        derivative = (1 / n) * alpha * np.dot(x.T, error)
+        weights = weights - derivative
 
-    for i in range(n):
-        dj_dw0, dj_dw1, dj_dw2 = gradients(x1, x2, y, w0, w1, w2)
+        weightList.append(weights)
+        costList.append(cost)
 
-        # cost = cost + ((h - y[i]) ** 2)
-        w0 = w0 - (alpha * dj_dw0)
-        w1 = w1 - (alpha * dj_dw1)
-        w2 = w2 - (alpha * dj_dw2)
-
-        p.append([w0, w1, w2])
-
-    return p
+    return weightList, costList
